cec9cf48230816d743b159270caa1af9d3f2a24a
The Boss <joanna.rudowald@gmail.com>
2025-01-12 15:11:45 +0100

    6.2 Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        }else{
+            System.out.println("test FAIL");
+        }
 
 
+        Calculator calculator = new Calculator(10, 7);
+
+        int addResult = calculator.add(calculator.getA(), calculator.getB());
+        int subResult = calculator.subtract(calculator.getA(), calculator.getB());
+
+        if (calculator.getA()+calculator.getB() == addResult) {
+            System.out.println("Calculator adding test OK");
+        } else {
+            System.out.println("Calculator adding test FAIL");
+        }
+
+        if (calculator.getA()-calculator.getB() == subResult) {
+            System.out.println("Calculator subtracting test OK");
+        } else {
+            System.out.println("Calculator subtracting test FAIL");
+        }
     }
 }
